e401e84cd895815b76fe0ed6b09d3305b5f0925e
AKNursumar <a.k.nursumar@gmail.com>
2026-06-08 19:53:04 +0530

    Model Serializer

---
+++ gs6/myapp.py
@@ -0,0 +1,58 @@
+import requests
+import json
+
+URL = 'http://127.0.0.1:8000/student_api/'
+
+
+# Read Data
+def get_data(id = None):
+    data = {}
+    if id is not None:
+        data = {'id': id}
+    json_data = json.dumps(data)
+    r = requests.get(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+# get_data()
+
+# create data
+def post_data():
+    data = {
+        'name': 'Ravi',
+        'roll':103,
+        'city':'Dhanbad'
+    }
+
+    json_data = json.dumps(data)
+    r = requests.post(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+# post_data()
+
+# Updata Data
+def update_data():
+    data = {
+        'id':6,
+        'name': 'Rohit',
+        'city':'Gondal'
+    }
+
+    json_data = json.dumps(data)
+    r = requests.put(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+# update_data()
+
+#
+# Deleting Data
+def delete_data():
+    data = { 'id': 6}
+    json_data = json.dumps(data)
+    r = requests.delete(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+delete_data()
