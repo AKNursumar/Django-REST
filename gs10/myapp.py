ff82504a593e41cdc891956045ba4772359c8f90
AKNursumar <a.k.nursumar@gmail.com>
2026-06-12 17:51:07 +0530

    CRUD API using Function based API view

---
+++ gs10/myapp.py
@@ -0,0 +1,62 @@
+import requests
+import json
+
+URL = 'http://127.0.0.1:8000/student_api/'
+
+
+# Read Data
+def get_data(id = None):
+    data = {}
+    if id is not None:
+        data = {'id': id}
+    headers = {'Content-Type': 'application/json'}
+    json_data = json.dumps(data)
+    r = requests.get(url=URL,headers=headers, data=json_data)
+    data = r.json()
+    print(data)
+
+# get_data()
+
+
+# create data
+def post_data():
+    data = {
+        'name':'Mohit',
+        'roll':104,
+        'city':'Dhanbad'
+    }
+
+    headers = {'Content-Type': 'application/json'}
+
+    json_data = json.dumps(data)
+    r = requests.post(url=URL,headers=headers ,data=json_data)
+    data = r.json()
+    print(data)
+
+# post_data()
+
+# Updata Data
+def update_data():
+    data = {
+        'id':4,
+        'name': 'Rohit',
+        'city':'Gondal'
+    }
+    headers = {'Content-Type': 'application/json'}
+    json_data = json.dumps(data)
+    r = requests.put(url=URL,headers=headers ,data=json_data)
+    data = r.json()
+    print(data)
+
+# update_data()
+
+# Deleting Data
+def delete_data():
+    data = { 'id': 3 }
+    headers = {'Content-Type': 'application/json'}
+    json_data = json.dumps(data)
+    r = requests.delete(url=URL, headers=headers,data=json_data)
+    data = r.json()
+    print(data)
+
+delete_data()
